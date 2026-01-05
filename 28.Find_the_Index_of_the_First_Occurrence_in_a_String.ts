10b5d5b5eea5adfc84256b65483c93129546e6e5
walid-hud <walidthehud@gmail.com>
2026-01-05 17:30:29 +0100

    28 redacted

--- 28.Find_the_Index_of_the_First_Occurrence_in_a_String.ts
+++ 28.Find_the_Index_of_the_First_Occurrence_in_a_String.ts
@@ -1,18 +1,1 @@
-function strStr(haystack: string, needle: string): number {
-    for(let i = 0; i<haystack.length;i++){
-        if(haystack[i]===needle[0]){
-            for(let j = 0; j<needle.length;j++){
-                if(needle[j]!==haystack[j]){
-                    return -1
-                }
-            }
-            return i
-            
-        }
-    }
-    return -1
-
-};
-
-console.assert(strStr("sadbutsad" , "sad")===0)
-console.assert(strStr("leetcode" , "leeto")===-1)
+// requires further search
